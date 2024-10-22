fb73c567c763639033e736ec529ec396fe105895
Martin <martinfl.dev@gmail.com>
2024-10-21 21:56:20 -0300

    Update Database.java

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:postgresql://localhost:5432/8_escalones"; // Cambia el nombre de la base de datos
-    private static final String USER = "postgres"; // Cambia tu usuario
-    private static final String PASSWORD = "123123"; // Cambia tu contraseña
+    //Aca traten de que coincida con su db en postgre
+    private static final String URL = "jdbc:postgresql://localhost:5432/8_escalones";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
