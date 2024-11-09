a2422e68e3769cf720fd9aa897fa213dacb9e950
Axel Rojas <89797191+AxelRojas-hub@users.noreply.github.com>
2024-11-09 10:15:01 -0300

    Fixes ronda final

--- src/main/java/model/logica/RondaFinal.java
+++ src/main/java/model/logica/RondaFinal.java
@@ -8,36 +8,40 @@ import model.PreguntaOpcion;
 public class RondaFinal implements EstadoRonda {
     @Override
     public void rondaDePreguntas(Ronda ronda,List<Participante> participantes) {
-        //Hay que ver como es la logica de la ronda final.
-        //Hay que manejar los errores y aciertos de los participantes.
         //La base de datos deberá tener un tema llamado Final que junte todas las preguntas, para hacer preguntas de todos los temas.
         PreguntaOpcion preg;
         String resp;
         Integer cantPreguntasRestantes = 10;
         
         System.out.println("Ronda de preguntas Final");
-         
+        
         for (int i = 0; i < 10; i++){ 
-            for (Participante par:participantes) {
-            preg=par.getPreguntasParticipante().get(i);
-            resp=par.getRespuestaParticipante();
-            if (preg.getRespuestaCorrecta()==resp){
-                par.setCantAciertos(1);
+            for (Participante participante:participantes) {
+            preg=participante.getPreguntasParticipante().get(i);
+            resp=participante.getRespuestaParticipante();
+            if (preg.getRespuestaCorrecta().equals(resp)){
+                participante.sumaAcierto();
             }else {
-                par.setCantErrores(1);
+                participante.sumaError();
                 }
             }
             
-            cantPreguntasRestantes -= i;
+            cantPreguntasRestantes--;
             
             //Verifica si uno de los dos participantes ya no tiene posiblidad de remontar y termina la ronda final.
             if (participantes.getFirst().getCantAciertos() > participantes.getLast().getCantAciertos() + cantPreguntasRestantes
-             || participantes.getLast().getCantAciertos() > participantes.getFirst().getCantAciertos() + cantPreguntasRestantes ) {
+            || participantes.getLast().getCantAciertos() > participantes.getFirst().getCantAciertos() + cantPreguntasRestantes ) {
+                // Capaz falta un estado para cuando termina el juego
+                // Lo podemos manejar desde el controlador igual
                 break;
             }
-
+            // Verifica si ambos participantes tienen la misma cantAciertos y no hay pregs restantes
+            if (cantPreguntasRestantes == 0 
+            && participantes.getFirst().getCantAciertos() == participantes.getLast().getCantAciertos()) {
+                System.out.println("Pasa a ronda de empate");
+                ronda.setRondaDeEmpate(participantes);
+                break;
+            }
         }
-
     }
-    
 }
