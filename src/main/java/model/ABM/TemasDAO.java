f8bde971021d6ba56ceb78fce6e6701247518370
nico <bustamantenicolas100@gmail.com>
2024-11-11 22:08:32 -0300

    Agrego metodo en temasDAO para usar en vista
    agrego metodo para que liste con la visual

--- src/main/java/model/ABM/TemasDAO.java
+++ src/main/java/model/ABM/TemasDAO.java
@@ -96,4 +96,33 @@ public class TemasDAO implements DAO<Tema>{
             System.err.println("Error al modificar tema: " + e.getMessage());
         }
     }
+
+    public List<Tema> obtenerTemasConCantidadPreguntas() {
+        List<Tema> temas = new ArrayList<>();
+
+        String query = "SELECT t.id_tema AS ID, t.nombre_tema AS Tema, " +
+                       "COUNT(p.id_pregunta) AS \"Cantidad de preguntas\" " +
+                       "FROM Tema t " +
+                       "LEFT JOIN preguntas p ON t.id_tema = p.id_tema " +
+                       "GROUP BY t.id_tema, t.nombre_tema";
+
+        try (Connection conn = Database.getInstance().getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                Tema tema = new Tema();
+                tema.setId(rs.getInt("ID"));
+                tema.setNombre(rs.getString("Tema"));
+                tema.setCantidadPreguntas(rs.getInt("Cantidad de preguntas"));
+                temas.add(tema);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return temas;
+    }
+
+    
 }
