b71c6ad9bf5f9c7d7cdb7e6b0641271239dce507
Flexxing0 <aldo53titos@gmail.com>
2024-11-13 19:19:21 -0300

    .

--- src/main/java/model/ABM/ParticipantesDAO.java
+++ src/main/java/model/ABM/ParticipantesDAO.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Participante;
+import model.Tema;
 
 public class ParticipantesDAO implements DAO<Participante> {
 
