012286baae7c083f4a3673dd5b0c8f23abe46b3c
Martin <martinfl.dev@gmail.com>
2024-11-23 23:36:53 -0300

    Agrego comportamiento al olvidar contraseña

--- src/main/java/controller/ControladorLogin.java
+++ src/main/java/controller/ControladorLogin.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
 
 import model.ABM.AdminDAO;
+import model.Admin;
 import view.VistaLogin;
 import view.VistaPrincipal;
 import model.ABM.ManagerSession;
@@ -30,39 +31,69 @@ public class ControladorLogin implements ActionListener{
 		this.vista.getBtnLogin().addActionListener(this);
 		this.vista.getBtnSalir().addActionListener(e -> {this.vista.dispose();});
 		
-		this.vista.getBtnOlvidoContrasenia().addActionListener(e -> {this.vista.getPanelCambio().setVisible(true);});
+		this.vista.getBtnOlvidoContrasenia().addActionListener(e -> {
+			this.vista.getPanelCambio().setVisible(true);
+		});
 		this.vista.getBtnCambioSalir().addActionListener(e -> {this.vista.getPanelCambio().setVisible(false);});
+		this.vista.getBtnCambioAceptar().addActionListener(e -> {
+			String nuevoUsuario = this.vista.getTxtCambioCuenta().getText();
+			String nuevaContrasenia1 = this.vista.getTxtContraseniaNueva1().getText();
+			String nuevaContrasenia2 = this.vista.getTxtContraseniaNueva2().getText();
+
+			Admin antiguoAdmin = this.adminDAO.buscarAdmin();
+
+			if (!antiguoAdmin.getNombre().equals(nuevoUsuario)) {
+				Notifications.getInstance().setJFrame(controladorPrincipal.getVista());
+				Notifications.getInstance().show(
+						Notifications.Type.ERROR,
+						Notifications.Location.TOP_CENTER,
+						"El nombre de usuario no coincide con el actual. Por favor, ingrese su nombre de usuario correctamente."
+				);
+				return;
+			}
+			if (!nuevaContrasenia1.equals(nuevaContrasenia2)) {
+				Notifications.getInstance().setJFrame(controladorPrincipal.getVista());
+				Notifications.getInstance().show(
+						Notifications.Type.ERROR,
+						Notifications.Location.TOP_CENTER,
+						"Las contraseñas no coinciden. Por favor, intente nuevamente."
+				);
+				return;
+			}
+
+			Admin nuevoAdmin = new Admin(antiguoAdmin.getNombre(), nuevaContrasenia1);
+
+			this.adminDAO.modificar(nuevoAdmin);
+
+			Notifications.getInstance().setJFrame(controladorPrincipal.getVista());
+			Notifications.getInstance().show(
+					Notifications.Type.SUCCESS,
+					Notifications.Location.TOP_CENTER,
+					"Contraseña cambiada con éxito."
+			);
+
+			this.vista.dispose();
+			this.vista.getPanelCambio().setVisible(false);
+		});
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		if (e.getSource() == vista.getBtnLogin()) {
             String cuenta = vista.getTxtCuenta().getText();
             String contrasenia = new String(vista.getTxtContrasenia().getPassword());
 
-            // Validar login con las credenciales
             if (adminDAO.Login(cuenta, contrasenia)) {
-                // Si el login es exitoso, mostrar vista principal
                 Notifications.getInstance().show(Notifications.Type.SUCCESS, Notifications.Location.TOP_CENTER, "Te has logueado correctamente");
                 Notifications.getInstance().setJFrame(controladorPrincipal.getVista());
 				ManagerSession.setLogueado(true);
 				controladorPrincipal.verificarEstadoSesion();
 				controladorPrincipal.getVista().actualizarEstadoLogin(true);
-                vista.dispose(); // Cierra la ventana de login
+                vista.dispose();
             } else {
-                // Si el login falla
             	Notifications.getInstance().show(Notifications.Type.ERROR, Notifications.Location.TOP_CENTER, "Credenciales incorrectas");
             	Notifications.getInstance().setJFrame(this.vista);
             }
         }
-
-		
 	}
-
-	
-
-
-
-	
 }
