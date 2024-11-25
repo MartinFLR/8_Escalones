350b6a1eba8e5d61c034e4f9ef4e733e42c8faac
Martin <martinfl.dev@gmail.com>
2024-11-25 16:54:45 -0300

    agrego eliminar del ranking

--- src/main/java/controller/ControladorRanking.java
+++ src/main/java/controller/ControladorRanking.java
@@ -1,30 +1,84 @@
 package controller;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
 import model.Participante;
 import model.ABM.ParticipantesDAO;
+import raven.toast.Notifications;
 import view.VistaRanking;
+
+import javax.swing.*;
 
 public class ControladorRanking implements ActionListener{
 
 	private VistaRanking vista;
-	private ParticipantesDAO participante;
+	private ParticipantesDAO participantesDAO;
 	
 	public ControladorRanking () {
 		this.vista = new VistaRanking (this);
 		this.vista.setVisible(true);
-		this.participante = new ParticipantesDAO();
+		this.participantesDAO = new ParticipantesDAO();
+		inicializarBotones();
 		this.vista.getBtnSalir().addActionListener(e -> {this.vista.dispose();});
 		this.rellenarTablas();
 	}
 
+	private void inicializarBotones(){
+
+		this.vista.getBtnEliminarRanking().addActionListener(e -> {
+			JDialog dialogoEliminar = new JDialog();
+			dialogoEliminar.setSize(300, 150);
+			dialogoEliminar.setLayout(new GridLayout(3, 1));
+			dialogoEliminar.setLocationRelativeTo(null);
+			dialogoEliminar.setModal(true);
+
+			JLabel lblEliminar = new JLabel("¿Esta seguro de eliminar el participante?");
+			JButton btnAceptar = new JButton("Aceptar");
+			JButton btnSalir = new JButton("Salir");
+
+			btnAceptar.addActionListener(ev -> {
+				int filaSeleccionada = this.vista.getTable().getSelectedRow();
+				if (filaSeleccionada != -1) {
+					try {
+						int idParticipante = Integer.parseInt(this.vista.getTable().getValueAt(filaSeleccionada, 0).toString());
+						System.out.println(idParticipante);
+
+						participantesDAO.eliminar(idParticipante);
+
+						dialogoEliminar.dispose();
+						Notifications.getInstance().show(Notifications.Type.SUCCESS, Notifications.Location.TOP_CENTER, "Categoría eliminada exitosamente");
+						Notifications.getInstance().setJFrame(vista);
+
+						// Actualiza la tabla para reflejar los cambios
+						this.rellenarTablas();
+					} catch (NumberFormatException ex) {
+						Notifications.getInstance().show(Notifications.Type.ERROR, Notifications.Location.TOP_CENTER, "Error al convertir el ID de la categoría a un número");
+						Notifications.getInstance().setJFrame(vista);
+					}
+				} else {
+					Notifications.getInstance().show(Notifications.Type.INFO, Notifications.Location.TOP_CENTER, "Seleccione una categoría a eliminar");
+					Notifications.getInstance().setJFrame(vista);
+				}
+			});
+
+
+			btnSalir.addActionListener(ev -> {
+				dialogoEliminar.setVisible(false);
+			});
+
+			dialogoEliminar.add(lblEliminar);
+			dialogoEliminar.add(btnAceptar);
+			dialogoEliminar.add(btnSalir);
+			dialogoEliminar.setVisible(true);
+		});
+	}
+
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	public void rellenarTablas() {
@@ -45,14 +99,4 @@ public class ControladorRanking implements ActionListener{
 		this.vista = vista;
 	}
 
-	public ParticipantesDAO getParticipante() {
-		return participante;
-	}
-
-	public void setParticipante(ParticipantesDAO participante) {
-		this.participante = participante;
-	}
-	
-	
-	
 }
