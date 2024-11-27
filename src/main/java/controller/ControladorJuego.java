63d827f80329dc00edca6a825dc4429ee1afa3b2
Nicolas Montiel <montielnicolas848@gmail.com>
2024-11-26 21:38:21 -0300

    termina la ronda final cuando ya no haya chance de remontar

--- src/main/java/controller/ControladorJuego.java
+++ src/main/java/controller/ControladorJuego.java
@@ -35,6 +35,7 @@ public class ControladorJuego implements ActionListener, KeyListener {
     private List<String> respuestasJugador;
     private final HashMap<Integer, Color> colorEscalon = new HashMap<>();
     private ReproductorPrincipal rp;
+    private int preguntasRestantes = 20; //20 ya que son las 10 mismas preguntas para cada uno
 
 	public ControladorJuego(Escalon escalon) {
 		this.escalon = escalon;
@@ -379,6 +380,8 @@ public class ControladorJuego implements ActionListener, KeyListener {
     
                 // Eliminar la pregunta actual  del participante
                 p.getPreguntasParticipante().remove(0);
+                preguntasRestantes--;
+                System.out.println("preguntas restantes: " + preguntasRestantes);
             }
         // Reiniciar la lista de respuestas para la siguiente ronda
             for (int i = 0; i < this.escalon.getParticipantes().size(); i++) {
@@ -386,6 +389,15 @@ public class ControladorJuego implements ActionListener, KeyListener {
             }
             if (turnoJugador >= escalon.getParticipantes().size()) {
                 turnoJugador = 0;
+            }
+
+            if (preguntasRestantes % 2 == 0) { // Verifica cuando sea par ya que me asegura que respondieron los 2
+                if (escalon.getParticipantes().get(0).getCantAciertos() > escalon.getParticipantes().get(1).getCantAciertos() + (preguntasRestantes/2) ||
+                escalon.getParticipantes().get(1).getCantAciertos() > escalon.getParticipantes().get(0).getCantAciertos() + (preguntasRestantes/2)) { 
+                    verificarRondaFinalYGanador();
+                } else {
+                    mostrarPreguntaActual();
+                }
             }
             if (!this.escalon.getParticipantes().get(turnoJugador).getPreguntasParticipante().isEmpty()) {
                 mostrarPreguntaActual();
